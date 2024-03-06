214fd343eb4c8275f2a89e53b583a06a525d6d3c
Isaac Vieira <isaacvf.dev@gmail.com>
2024-03-06 18:12:07 -0300

    Added hero img left and starting banner section

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import heroImg from "../../../public/assets/hero-img.png"
+import heroImgRight from "../../../public/assets/hero-img.png"
+import heroImgLeft from "../../../public/assets/hero-img-left.png"
 import {motion, useScroll, useTransform} from "framer-motion"
 
 export default function Hero() {
@@ -8,18 +9,28 @@ export default function Hero() {
     target: ref,
     offset: ["start start", "end start"]
   })
-  const imageY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+  const imageXLeft = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"])
+  const imageXRight = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+  
+  
 
   return (
     <header
       ref={ref} 
       className="relative h-24 lg:h-56 bg-indigo-50 flex justify-center items-center z-10 overflow-hidden">
       <motion.img
-        style={{x: imageY}} 
-        src={heroImg} 
+        style={{x: imageXLeft}}
+        src={heroImgLeft} 
+        alt="icons" 
+        className="h-24 lg:h-56 absolute top-0 left-0 object-cover"
+      />
+      <motion.img
+        style={{x: imageXRight}} 
+        src={heroImgRight} 
         alt="icons" 
         className="h-24 lg:h-56 absolute top-0 right-0 object-cover"
       />
+      
       <h1 className="text-slate-800 text-2xl lg:text-5xl max-w-36 lg:max-w-full text-center">Component Library</h1>
     </header>
   )
