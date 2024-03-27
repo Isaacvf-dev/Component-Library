71d660471d962a14a950e386c439dd0ed1357e67
Isaac Vieira <isaacvf.dev@gmail.com>
2024-03-27 15:35:06 -0300

    Fixed photo url

--- src/components/testimonials/TestimonialSection.jsx
+++ src/components/testimonials/TestimonialSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SectionContainer from "../SectionContainer";
 import SubSection from "../SubSection";
 import Testimonial from "./Testmonial";
+import ctoImg from "/assets/cto-photo.jpg"
 
 export default function TestimonialSection() {
   return (
@@ -11,7 +12,7 @@ export default function TestimonialSection() {
           text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos velit autem impedit sed architec.'}
           author={'May Andersons'}
           role={'Workcation, CTO'}
-          pic={'https://s3-alpha-sig.figma.com/img/ebbd/218e/43da4fc0a30598c1777461ca480d0fde?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FwR9foCwmUySweBS~NgcypaI~erZvawKDB-~tgS1RzAEk2~GnllgCL7g9kZjdueezHvw9xvJg4ssvbcfcLYj8NkOgv0W~k9UVm~vD~8z3MMTxfEoJI09A8CWCXGU8Fx5DVSrpVvEtMcgDReeDJV~1ytNeexNPWdo0pDwXKbnfgV0B39~uG3ILp1HWL0rZ6e7Y~htN0zb8Gq3TrRx4I8zRy55mUOscYQ~YvsOch6sNH~mKPFSMmqgsI8e69QoEi29svN89RraCLp1AnL5rXj9RRh2mDYsL-eGKuZMjZ2HsT1tT33LyMCtHm6Dim06deM1QVzp7HvCfhJUeVZuOKW3Lw__'}
+          pic={ctoImg}
         />          
         
       </SubSection>
